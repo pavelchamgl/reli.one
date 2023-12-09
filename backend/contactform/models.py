ee706abe8d0879951cfb18bea18d539a160daa72
CuperEvgeha1400 <novapiple228@gmail.com>
2023-12-10 01:20:05 +0400

    add name in models.py

--- backend/contactform/models.py
+++ backend/contactform/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Contact(models.Model):
+    name = models.CharField(max_length=255)
     email = models.EmailField()
     address = models.CharField(max_length=255)
     company_name = models.CharField(max_length=255, blank=True, null=True)
