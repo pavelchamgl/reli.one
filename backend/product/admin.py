92900640a137f381444f66605120813278d15b95
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2024-04-01 20:00:57 +0300

    Added license file model in adminpanel

--- backend/product/admin.py
+++ backend/product/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
-from .models import BaseProduct, ParameterName, ParameterValue, BaseProductImage, Category
+from .models import BaseProduct, ParameterName, ParameterValue, BaseProductImage, Category, LicenseFile
 
+
+class LicenseFileInline(admin.TabularInline):
+    model = LicenseFile
 
 
 class AdminBaseProduct(admin.ModelAdmin):
     list_display = ('name', 'product_description', 'price')
     list_filter = ['name', 'product_description', 'price']
     search_fields = ['name']
+    inlines = [LicenseFileInline]
+
 
 admin.site.register(BaseProduct, AdminBaseProduct)
 admin.site.register(Category)
