cd1356d7b4acc901569b15d918ed2644f8b1387d
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2024-04-09 22:34:18 +0300

    Added favorite model

--- backend/favorites/models.py
+++ backend/favorites/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from account.models import User
+from product.models import BaseProduct
 
-# Create your models here.
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(BaseProduct, on_delete=models.CASCADE)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['user', 'product']
