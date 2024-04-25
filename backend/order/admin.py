43b154d47db932eddd4b6565d6d084e8964d2d82
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2024-04-25 10:28:23 +0300

    Added functionality of order delivery type and statuses

--- backend/order/admin.py
+++ backend/order/admin.py
@@ -1,24 +1,39 @@
 from django.contrib import admin
-from .models import OrderItem, OrderStatus
-from django.db import models
-from account.admin import MyUserAdmin
-from authemail.admin import EmailUserAdmin
-
-
-"""
+from .models import OrderItem, DeliveryType, OrderStatus, SelfPickupStatus
 
 
 class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'total_amount', 'order_date', 'delivery_type', 'order_status', 'self_pickup_status')
+    list_filter = ('delivery_type', 'order_status', 'self_pickup_status')
 
-    list_display = ('get_user_basket_address', 'quantity')
+    actions = ['sort_orders_by_date']
 
-    def get_user_basket_address(self, obj):
-        return obj.user_basket.user.adress
+    def sort_orders_by_date(self, request, queryset):
+        sorted_orders = queryset.order_by('-order_date')
+        self.message_user(request, f"Orders sorted by date")
+        return sorted_orders
 
-    get_user_basket_address.short_description = 'address'  # Название столбца в административной панели
+    sort_orders_by_date.short_description = "Sort selected orders by date"
+
+    actions = ['change_to_courier', 'change_to_self_pickup', 'cancel_orders']
+
+    def change_to_courier(self, request, queryset):
+        queryset.update(delivery_type=DeliveryType.objects.get(name='Courier'))
+
+    change_to_courier.short_description = 'Change selected to Courier'
+
+    def change_to_self_pickup(self, request, queryset):
+        queryset.update(delivery_type=DeliveryType.objects.get(name='Self Pickup'))
+
+    change_to_self_pickup.short_description = 'Change selected to Self Pickup'
+
+    def cancel_orders(self, request, queryset):
+        queryset.update(order_status=OrderStatus.objects.get(name='Cancelled'))
+
+    cancel_orders.short_description = 'Cancel selected orders'
 
 
-
-"""
-admin.site.register(OrderItem)
-
+admin.site.register(OrderItem, OrderItemAdmin)
+admin.site.register(DeliveryType)
+admin.site.register(OrderStatus)
+admin.site.register(SelfPickupStatus)
--- backend/order/serializers.py
+++ backend/order/serializers.py
@@ -1,15 +1,38 @@
 from rest_framework import serializers
-from .models import OrderItem, OrderStatus
 
-class OrderStatusSerializer(serializers.Serializer):
-    value = serializers.CharField(max_length=50)
-    display_name = serializers.CharField(max_length=50)
+from .models import OrderItem, DeliveryType, OrderStatus, SelfPickupStatus
+
+
+class EnumSerializer(serializers.ModelSerializer):
+    value = serializers.CharField(source='value')
+    display_name = serializers.CharField(source='name')
+
+    class Meta:
+        fields = ('value', 'display_name')
+        read_only_fields = ('value', 'display_name')
+
+
+class DeliveryTypeSerializer(EnumSerializer):
+    class Meta(EnumSerializer.Meta):
+        model = DeliveryType
+
+
+class OrderStatusSerializer(EnumSerializer):
+    class Meta(EnumSerializer.Meta):
+        model = OrderStatus
+
+
+class SelfPickupStatusSerializer(EnumSerializer):
+    class Meta(EnumSerializer.Meta):
+        model = SelfPickupStatus
+
 
 class OrderItemSerializer(serializers.ModelSerializer):
+    delivery_type = DeliveryTypeSerializer()
+    order_status = OrderStatusSerializer()
+    self_pickup_status = SelfPickupStatusSerializer()
+
     class Meta:
         model = OrderItem
         fields = '__all__'
         depth = 2
-
-
-
