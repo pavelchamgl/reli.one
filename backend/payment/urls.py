f2a4bd6eba163f93280481da82bb7bb2ed90fc3a
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2024-07-12 12:08:58 +0300

    Resolved .gitignore conflict

--- backend/payment/urls.py
+++ backend/payment/urls.py
@@ -1,16 +1,15 @@
 from django.urls import path
 
 from .views import (
-    create_checkout_session,
-    stripe_webhook,
+    CreateStripeCheckoutSession,
+    StripeWebhookHandler,
     CreatePayPalPaymentView,
     PayPalWebhookView,
-
 )
 
 urlpatterns = [
-    path('create-checkout-session/', create_checkout_session, name='create_checkout_session'),
-    path('stripe-webhook/', stripe_webhook, name='stripe_webhook'),
+    path('create-checkout-session/', CreateStripeCheckoutSession.as_view(), name='create_checkout_session'),
+    path('stripe-webhook/', StripeWebhookHandler.as_view(), name='stripe_webhook'),
     path('create-paypal-payment/', CreatePayPalPaymentView.as_view(), name='create_paypal_payment'),
     path('paypal-webhook/', PayPalWebhookView.as_view(), name='paypal_webhook'),
 ]
