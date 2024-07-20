e425c019a589197f96c8553f5b2a4e9acc45cf8b
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2024-07-20 13:55:03 +0300

    Updated field product model

--- backend/product/models.py
+++ backend/product/models.py
@@ -67,7 +67,9 @@ class BaseProduct(models.Model):
         max_digits=2,
         decimal_places=1,
         default=Decimal('0.0'),
-        validators=[MinValueValidator(Decimal('1.0')), MaxValueValidator(Decimal('5.0'))]
+        validators=[MinValueValidator(Decimal('1.0')), MaxValueValidator(Decimal('5.0'))],
+        blank=True,
+        null=True,
     )
     total_reviews = models.IntegerField(default=0)
 
