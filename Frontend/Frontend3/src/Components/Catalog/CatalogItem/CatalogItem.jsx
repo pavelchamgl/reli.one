1c65ad5e03a1713b5f5104c174eb0413990800df
dosa <almazbekovd12@gmail.com>
2024-08-07 03:04:22 +0600

    edit errors

--- Frontend/Frontend3/src/Components/Catalog/CatalogItem/CatalogItem.jsx
+++ Frontend/Frontend3/src/Components/Catalog/CatalogItem/CatalogItem.jsx
@@ -8,30 +8,43 @@ import arrRight from "../../../assets/Catalog/arrRight.svg";
 
 import styles from "./CatalogItem.module.scss";
 
-const CatalogItem = ({ data, handleClose }) => {
+const CatalogItem = ({
+  data,
+  handleClose,
+  catalogCategory,
+  setCatalogCategory,
+}) => {
   const isMobile = useMediaQuery({ maxWidth: 500 });
   const navigate = useNavigate();
   console.log(data);
 
   const { setCategory } = useActions();
 
-
   const handleClick = () => {
     if (isMobile) {
       navigate("/mob_category");
+      setCatalogCategory(data.name);
       setCategory(data);
       handleClose();
     } else {
+      setCatalogCategory(data.name);
       setCategory(data);
     }
   };
-
 
   return (
     <button onClick={handleClick} className={styles.main}>
       <div>
         {data && data?.image_url && <img src={data?.image_url} alt="" />}
-        <p>{data?.name}</p>
+        <p
+          className={
+            catalogCategory === data?.name
+              ? styles.categoryNameAcc
+              : styles.categoryName
+          }
+        >
+          {data?.name}
+        </p>
       </div>
       <button>
         <img src={arrRight} alt="" />
