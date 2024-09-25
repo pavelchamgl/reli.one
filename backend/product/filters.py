89a933598e06307009d4dde2f8c2f74db51ebd46
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2024-09-25 11:00:58 +0300

    Changed product filters

--- backend/product/filters.py
+++ backend/product/filters.py
@@ -1,12 +1,13 @@
 from django_filters import rest_framework as filters
 
-from .models import BaseProduct, Category
+from .models import BaseProduct
 
 
 class BaseProductFilter(filters.FilterSet):
-    min_price = filters.NumberFilter(field_name="price", lookup_expr='gte')
-    max_price = filters.NumberFilter(field_name="price", lookup_expr='lte')
+    min_price = filters.NumberFilter(field_name="min_price", lookup_expr='gte', label='Minimum Price')
+    max_price = filters.NumberFilter(field_name="min_price", lookup_expr='lte', label='Maximum Price')
+    rating = filters.NumberFilter(field_name='rating', lookup_expr='gte')
 
     class Meta:
         model = BaseProduct
-        fields = ['min_price', 'max_price']
+        fields = ['min_price', 'max_price', 'rating']
