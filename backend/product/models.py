f58ac7bede8112293704164461df3c4672a0539d
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2024-11-04 22:03:47 +0300

    Updated fild product model

--- backend/product/models.py
+++ backend/product/models.py
@@ -54,7 +54,7 @@ class BaseProduct(models.Model):
     rating = models.DecimalField(
         max_digits=2,
         decimal_places=1,
-        # default=Decimal('1.0'),
+        default=Decimal('1.0'),
         validators=[MinValueValidator(Decimal('1.0')), MaxValueValidator(Decimal('5.0'))],
         blank=True,
         null=True,
