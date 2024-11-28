11f3b02cd6bbd2beca16e34c8c0e74b16a37cd11
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2024-11-28 15:47:05 +0300

    Changed signal for product rating

--- backend/product/signals.py
+++ backend/product/signals.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
 from django.dispatch import receiver
+from django.db.models import Avg
 from django.db.models.signals import post_save
 
 from reviews.models import Review
@@ -8,10 +9,16 @@ from reviews.models import Review
 @receiver(post_save, sender=Review)
 def update_product_rating_and_reviews(sender, instance, created, **kwargs):
     if created:
-        product = instance.product
-        reviews = Review.objects.filter(product=product)
-        total_reviews = reviews.count()
-        total_rating = sum(review.rating for review in reviews)
-        product.rating = round(Decimal(total_rating) / Decimal(total_reviews), 1) if total_reviews > 0 else Decimal('0.0')
-        product.total_reviews = total_reviews
+        product = instance.product_variant.product
+
+        reviews = Review.objects.filter(product_variant__product=product)
+
+        avg_rating = reviews.aggregate(average_rating=Avg('rating'))['average_rating'] or Decimal('0.0')
+
+        avg_rating = Decimal(avg_rating) if isinstance(avg_rating, float) else avg_rating
+
+        avg_rating = avg_rating.quantize(Decimal('0.0'))
+
+        product.rating = avg_rating
+        product.total_reviews = reviews.count()
         product.save()
