0b62bfd265e8439cb0d138eaec61773e40613f01
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2024-12-10 14:55:29 +0300

    Created seller profile model

--- backend/sellers/models.py
+++ backend/sellers/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from accounts.model import CustomUser
+from accounts.choices import UserRole
+
+
+class SellerProfile(models.Model):
+    user = models.OneToOneField(
+        CustomUser,
+        on_delete=models.CASCADE,
+        limit_choices_to={'role': UserRole.SELLER},
+        related_name='seller_profile'
+    )
+    managers = models.ManyToManyField(
+        CustomUser,
+        limit_choices_to={'role': UserRole.MANAGER},
+        related_name='sellers_managed',
+        blank=True
+    )
+
+    def __str__(self):
+        return f"SellerProfile: {self.user.email}"
