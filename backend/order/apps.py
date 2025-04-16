2509cfa9c7f82af0e647e9ceeacbfb9cb63db00c
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2025-04-16 17:39:22 +0300

    Disabled email sending signal when creating an order

--- backend/order/apps.py
+++ backend/order/apps.py
@@ -5,8 +5,3 @@ from django.core.signals import setting_changed
 class OrderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'order'
-
-    def ready(self):
-        from .signals import send_order_confirmation_email
-        setting_changed.connect(send_order_confirmation_email)
-
