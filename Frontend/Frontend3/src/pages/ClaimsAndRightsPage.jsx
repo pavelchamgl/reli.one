931701f19267d828d84cf19b9969a000cfe9d64a
dosa <almazbekovd12@gmail.com>
2026-03-16 15:42:43 +0600

    add new pages and redirects

---
+++ Frontend/Frontend3/src/pages/ClaimsAndRightsPage.jsx
@@ -0,0 +1,51 @@
+import { useTranslation } from "react-i18next"
+import { useNavigate } from "react-router-dom"
+
+import formular from "../../public/Formulář pro uplatnění reklamace Reli.one (1).pdf"
+
+import Container from "../ui/Container/Container"
+
+import styles from "../styles/ClaimsAndRightsPage.module.scss"
+
+const ClaimsAndRightsPage = () => {
+
+    const navigate = useNavigate()
+    const { t } = useTranslation('claim')
+
+    return (
+        <div>
+            <div className={styles.titleWrap}>
+                <h1>{t('claim.title')}</h1>
+            </div>
+            <Container>
+                <div className={styles.mainWrap}>
+                    <div className={styles.block}>
+                        <p>{t('claim.text1')}</p>
+                        <p>{t('claim.text2')}</p>
+                        <p>{t('claim.text3')}</p>
+                        <p>{t('claim.text4')}</p>
+                    </div>
+                    <div className={styles.btnWrap}>
+                        <button
+                            onClick={() => navigate('/terms')}
+                            className={styles.yellBtn}>
+                            {t('claim.moreInfo')}
+                        </button>
+                        <a href={formular} download>
+                            <button className={styles.bordBtn}>
+                                {t('claim.claimForm')}
+                            </button>
+                        </a>
+                    </div>
+                    <p className={styles.afterDesc}>
+                        {t('claim.afterDesc')}
+                    </p>
+                </div>
+
+
+            </Container>
+        </div>
+    )
+}
+
+export default ClaimsAndRightsPage
---
+++ Frontend/Frontend3/src/pages/Withdrawal.jsx
@@ -0,0 +1,73 @@
+import { useTranslation } from "react-i18next"
+import { useNavigate } from "react-router-dom"
+
+import Container from "../ui/Container/Container"
+import timeIc from "../assets/claim/time.svg"
+import file from "../../public/Odstoupení od smlouvy a vrácení zboží reli.one (1).pdf"
+
+import styles from "../styles/ClaimsAndRightsPage.module.scss"
+
+const Withdrawal = () => {
+
+    const navigate = useNavigate()
+    const { t } = useTranslation('claim')
+
+    return (
+        <div>
+            <div className={styles.titleWrap}>
+                <h1>{t('withdawal.title')}</h1>
+            </div>
+            <Container>
+                <div className={styles.mainWrap}>
+                    <div className={styles.block}>
+                        <p>{t('withdawal.text1')}</p>
+                        <p>{t('withdawal.text2')}</p>
+                        <p>{t('withdawal.text3')}</p>
+                        <p>{t('withdawal.text4')}</p>
+                    </div>
+                    <div className={styles.dayReturnBlock}>
+                        <div className={styles.iconWrap}>
+                            <img src={timeIc} alt="" />
+                        </div>
+                        <div>
+                            <h3><span>14</span>{t('withdawal.blockTitle')}</h3>
+                            <p>
+                                {t('withdawal.blockDesc')}
+                                <span>14</span>
+                                {t('withdawal.afterDesc')}
+                            </p>
+                        </div>
+                    </div>
+                    <div className={styles.btnWrap}>
+                        <button
+                            onClick={() => navigate('/terms')}
+                            className={styles.yellBtn}>
+                            {t('withdawal.moreInfo')}
+                        </button>
+                        <a href={file} download>
+                            <button className={styles.bordBtn}>
+                                {t('withdawal.withForm')}
+                            </button>
+                        </a>
+                    </div>
+
+                </div>
+
+
+            </Container>
+        </div>
+    )
+}
+
+export default Withdrawal
+
+// "withdawal": {
+//     "title": "",
+//     "text1": "",
+//     "text2": "",
+//     "text3": "",
+//     "text4": "",
+//     "moreInfo": "",
+//     "claimForm": "",
+//     "afterDesc": ""
+// }
